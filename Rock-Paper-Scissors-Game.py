16cc6b834a164cb57728a4215ba1af43c0ada81d
BHIM RAJBHAR <62300815+Bhim-Rajbhar@users.noreply.github.com>
2024-02-18 11:24:37 +0530

    Update Rock-Paper-Scissors-Game.py

--- Rock-Paper-Scissors-Game.py
+++ Rock-Paper-Scissors-Game.py
@@ -36,25 +36,31 @@ scissors = '''
 # List containing ASCII art for each choice
 game_images = [rock, paper, scissors]
 
-# Prompting the user to choose rock, paper, or scissors
+# User input for their choice (0 for Rock, 1 for Paper, 2 for Scissors)
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.\n"))
-print(game_images[user_choice])  # Displaying the user's choice using the corresponding ASCII art
 
-# Generating a random choice for the computer
-computer_choice = random.randint(0, 2)
-print("Computer chose:")
-print(game_images[computer_choice])  # Displaying the computer's choice using the corresponding ASCII art
-
-# Logic for determining the winner
+# Validation of user input
 if user_choice >= 3 or user_choice < 0: 
-    print("You typed an invalid number, you lose!")  # If the user input is invalid
-elif user_choice == 0 and computer_choice == 2:
-    print("You win!")  # If user chooses Rock and computer chooses Scissors
-elif computer_choice == 0 and user_choice == 2:
-    print("You lose")  # If computer chooses Rock and user chooses Scissors
-elif computer_choice > user_choice:
-    print("You lose")  # If computer's choice beats user's choice
-elif user_choice > computer_choice:
-    print("You win!")  # If user's choice beats computer's choice
-elif computer_choice == user_choice:
-    print("It's a draw")  # If both choices are the same, it's a draw
+    # If the user input is invalid
+    print("You typed an invalid number, you lose!")  
+else:
+    # Displaying the user's choice
+    print(game_images[user_choice])  
+
+    # Generating computer's choice randomly
+    computer_choice = random.randint(0, 2)
+    print("Computer chose:")
+    # Displaying the computer's choice
+    print(game_images[computer_choice])  
+
+    # Determining the winner
+    if user_choice == 0 and computer_choice == 2:
+        print("You win!")  
+    elif computer_choice == 0 and user_choice == 2:
+        print("You lose")  
+    elif computer_choice > user_choice:
+        print("You lose")  
+    elif user_choice > computer_choice:
+        print("You win!")  
+    elif computer_choice == user_choice:
+        print("It's a draw")  
